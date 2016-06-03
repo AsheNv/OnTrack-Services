a9fa6c577f26446450ce63c7ce2505ca9487f736
Ashen Priyankara <AsheNv@users.noreply.github.com>
2016-06-03 08:38:27 +0530

    Create Connection.php

---
+++ Connection.php
@@ -0,0 +1,10 @@
+<?php
+
+define ('hostname', 'localhost');
+define ('user', 'hike_user');
+define ('password', 'user123');
+define ('databaseName', 'hike_ontrack');
+
+$connect = mysqli_connect(hostname, user, password, databaseName);
+
+
